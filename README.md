b84b57d0c4199954a30a53781cf116a963f716dc
wralitera <wralitera@wynd.eu>
2020-03-13 23:16:56 +0100

    adding new test run

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ###### For more question, contact: 
 `RALITERA Rija William`
+
+## Run the test
+make run_testname_headless BASE_URL=http://www.google.com/ TEST_RUN_NAME=TEST
--- cypress/integration/tests/formation/Formation_test.js
+++ cypress/integration/tests/formation/Formation_test.js
@@ -3,7 +3,7 @@ describe('Formation', function () {
     this.actionwords = Object.create(require('../../actionwords.js').Actionwords);
   });
 
-  it('Formation', function () {
+  it('Formation (uid:ba4f120b-a9b9-49b3-ba98-f2fd9bc50ab6)', function () {
     // Navigate to Google page
     // Given I navigate to "http://www.google.com/"
     this.actionwords.iNavigateToP1("http://www.google.com/");
