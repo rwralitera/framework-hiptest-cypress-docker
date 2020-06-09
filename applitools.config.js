fb6ec80dabb89e50d30a7026228dd185c31a2227
bentrole <william.ralitera@gmail.com>
2020-06-10 00:30:25 +0200

    Test Applitools With Cypressio

--- applitools.config.js
+++ applitools.config.js
@@ -4,15 +4,15 @@ module.exports = {
     //
     browser: [
         // Add browsers with different viewports
-        {width: 800, height: 600, name: 'chrome'},
-        {width: 700, height: 500, name: 'firefox'},
-        {width: 1600, height: 1200, name: 'ie11'},
-        {width: 1024, height: 768, name: 'edgechromium'},
-        {width: 800, height: 600, name: 'safari'},
+        {width: 1024, height: 768, name: 'chrome'},
+        // {width: 700, height: 500, name: 'firefox'},
+        // {width: 1600, height: 1200, name: 'ie11'},
+        // {width: 800, height: 600, name: 'edgechromium'},
+        // {width: 800, height: 600, name: 'safari'},
         // Add mobile emulation devices in Portrait mode
-        {deviceName: 'iPhone X', screenOrientation: 'portrait'},
-        {deviceName: 'Pixel 2', screenOrientation: 'portrait'}
+        // {deviceName: 'iPhone X', screenOrientation: 'portrait'},
+        // {deviceName: 'Pixel 2', screenOrientation: 'portrait'}
     ],
     // set batch name to the configuration
-    batchName: 'Ultrafast Batch'
+    batchName: 'Applitools test with cypress (Gitlab-CI + CircleCI)'
 }
