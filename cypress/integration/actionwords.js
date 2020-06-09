ac7800c7cbe83ff0a88b7d08798651619ed57edb
bentrole <william.ralitera@gmail.com>
2020-06-10 01:15:34 +0200

    renaming gitlab-ci.yml

--- cypress/integration/actionwords.js
+++ cypress/integration/actionwords.js
@@ -6,7 +6,7 @@ exports.Actionwords = {
     cy.get('.name').contains(p3).first().click()
     cy.wait(5000) //Modification de la page après rafraichissement en 2 temps
     cy.get('.ember-view').contains(p1).click()
-    cy.eyesCheckWindow('theUserIsOnP1SectionOfP2PageForP3Company');
+    cy.eyesCheckWindow('theUserIsOn'+p1+'SectionOf'+p2+'PageFor'+p3+'Company');
   },
   theUserIsLoggedInAsP1: function (p1) {
     cy.visit('/login')
@@ -14,18 +14,18 @@ exports.Actionwords = {
     cy.get('#ember575').type('Rrw141189*')
     cy.get('#ember612').click()
     cy.get('#ember1044 > h1').should('exist')
-    cy.eyesCheckWindow('theUserIsLoggedInAsP1');
+    cy.eyesCheckWindow('theUserIsLoggedInAs'+p1);
 
   },
   theUserClicksOnP1Link: function (p1) {
     cy.get('.infos').contains(p1).click()
-    cy.eyesCheckWindow('theUserClicksOnP1Link');
+    cy.eyesCheckWindow('theUserClicksOn'+p1+'Link');
 
 
   },
   theUserShoulsSeeAFlagWithColorP1: function (p1) {
     cy.get('.scan-flag').should('have.class',p1)
-    cy.eyesCheckWindow('theUserShoulsSeeAFlagWithColorP1');
+    cy.eyesCheckWindow('theUserShoulsSeeAFlagWithColor'+p1);
 
   }
 };
